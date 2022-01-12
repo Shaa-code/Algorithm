0b5876ddfeaa69f51f0c19f418f6d2b7cf525f12
SeungYun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-01-12 19:03:09 +0900

    Create 수들의 합.py

---
+++ 파이썬 알고리즘 테스트/수들의 합.py
@@ -0,0 +1,41 @@
+내가 푼 방식
+import sys
+sys.stdin=open("input.txt","rt")
+
+N,M=map(int,input().split())
+A = list(map(int,input().split()))
+
+cnt = 0
+for i in range(N+1):
+    for j in range(i+1,N+1):
+        if sum(A[i:j]) == M:
+            cnt +=1
+print(cnt)
+
+강사님이 푼 방식
+
+n,m=map(int,input().split())
+a = list(map(int,input().split()))
+
+lt = 0
+rt = 1
+tot = a[0]
+cnt = 0
+while True:
+		if tot < m:
+				if rt < n:
+						tot += a[rt]
+						rt += 1
+				else:
+						break
+		elif tot == m:
+				cnt += 1
+				tot -= a[lt]
+				lt += 1
+		else:
+				tot -= a[lt]
+				lt += 1
+print(cnt)
+
+강사님이 푼 방식은 들어오는 수가 많아질때도,잘 작동하게끔 설계되었다.
+하지만, 내가 만든 방식은, 시간복잡도가 O(n^2)이라 들어오는 수가 많아지면, 속도가 크게 저하도된다.
