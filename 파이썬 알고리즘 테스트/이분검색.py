ee2bb42eafc9455bfcd00be2e1eb9728b0bb3462
SeungYun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-02-02 14:17:17 +0900

    Update 이분검색.py

--- 파이썬 알고리즘 테스트/이분검색.py
+++ 파이썬 알고리즘 테스트/이분검색.py
@@ -1,8 +1,9 @@
 내가 푼 코드
+
 import sys
 sys.stdin=open("input.txt","rt")
 
-n,k = map(int,input().split())
+n,m = map(int,input().split())
 lst = list(map(int,input().split()))
 
 #lst.sort()
@@ -13,24 +14,24 @@ print(lst)
 lt = 0
 rt = n-1
 mid = (lt+rt)//2
-while lst[mid]!=k:
-	if k < lst[mid]:
+while lst[mid]!=m:
+	if m < lst[mid]:
 		mid -= 1
 	else:
 		mid += 1
-print(mid)
+print(mid+1)
 
 강사님이 푼 코드
 
 lst.sort()
 lt=0
 rt=n-1
-while it<=rt:
-	mid=(lt+rt)//2
-	if lst[mid]==k:
+while lt<=rt:
+	mid = (lt+rt)//2
+	if lst[mid] == m:
 		print(mid+1)
 		break
-	elif a[mid]>m:
-		rt=mid-1
+	elif lst[mid] > m:
+  		rt = mid-1
 	else:
-		lt=mid+1
+  		lt = mid+1
