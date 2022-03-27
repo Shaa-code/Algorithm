e4ff738a1d9182beb1bee47bd65bf78259492f84
SeungYun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-03-28 03:32:34 +0900

    Create 문자찾기.java

---
+++ Java Coding Test/문자찾기.java
@@ -0,0 +1,19 @@
+class CodeTest{
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        System.out.print(T.solution(str,c));
+    }
+}
+
+class Main {
+    public int solution(String str, char c){
+        str = str.toLowerCase();
+        int cnt = 0;
+        for(int i = 0 ; i < str.length(); i++)
+            if (str.charAt(i) == c) cnt++;
+            return cnt;
+    }
+}
