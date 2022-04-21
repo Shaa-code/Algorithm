64a4bfc6891f3df1306c9498a31618054f6eb84a
SeungYun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-04-21 23:07:14 +0900

    Update 문장 속 단어.java

--- Java Coding Test/문장 속 단어.java
+++ Java Coding Test/문장 속 단어.java
@@ -21,3 +21,30 @@ class Main{
         return ret;
     }
 }
+
+/*
+class Main{
+
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(T.solution(str));
+    }
+
+    public String solution(String str) {
+        String ret = "";
+        int min = Integer.MIN_VALUE, pos;
+        while((pos=str.indexOf(' '))!=-1) {
+            String tmp = str.substring(0, pos);
+            int len = tmp.length();
+            if (len > min) {
+                min = len;
+                ret = tmp;
+            }
+            str = str.substring(pos+1);
+        }
+        return ret;
+    }
+}
+*/
