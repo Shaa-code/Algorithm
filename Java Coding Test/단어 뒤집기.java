f0baba13f139988cec887a8939e7bcdac8d4c90a
SeungYun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-04-22 23:37:33 +0900

    Create 단어 뒤집기.java

---
+++ Java Coding Test/단어 뒤집기.java
@@ -0,0 +1,25 @@
+class Main {
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+
+
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> ret = new ArrayList<>();
+        for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            ret.add(tmp);
+        }
+        return ret;
+    }
+}
