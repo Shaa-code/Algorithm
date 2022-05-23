4bc27319ee5a4f7ae3c21b03bacd33731088595b
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-05-24 00:21:31 +0900

    Create 뒤집은 소수.java

---
+++ Java Coding Test/뒤집은 소수.java
@@ -0,0 +1,37 @@
+class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.println(x + " ");
+        }
+    }
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+            if (isPrime(res)) ret.add(res);
+        }
+        return ret;
+    }
+
+    public boolean isPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
