6b36a84f16d014b43bdbe5ca2c89f709af726b73
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-05-28 21:51:16 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13824 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args){
+        Main T = new Main();
+        T.solution();
+    }
+    public void solution(){
+        System.out.println("Hello World!");
+    }
+}
