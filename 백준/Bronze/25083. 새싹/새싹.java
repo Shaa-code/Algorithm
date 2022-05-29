81befde2af646dc994ad104345494568fdc805ea
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-05-30 00:57:03 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.solution();
+    }
+    public void solution(){
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+        return;
+    }
+}
