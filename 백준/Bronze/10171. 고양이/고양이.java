219dfa0fa6a79c57fd511f0da314cdae1c47ce1d
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-05-30 00:50:55 +0900

    [Bronze V] Title: 고양이, Time: 132 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.solution();
+    }
+    public void solution(){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
