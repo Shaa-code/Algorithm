7d5d8fc148bd4069b869987efb0eeaa11c6a36ee
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-05-29 22:01:03 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.solution();
+    }
+    public void solution(){
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
