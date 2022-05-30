90db8cd704ec6907ee99a44b7aa0adf4567d57bf
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-05-30 16:51:36 +0900

    [Bronze V] Title: ??!, Time: 232 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        T.solution(str);
+    }
+
+    public void solution(String str){
+        System.out.println(str + "??!");
+    }
+}
