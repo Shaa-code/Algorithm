5c6cccd35842efd9d420caddec9e9c5c8267f9cf
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-06-07 02:27:03 +0900

    [Bronze V] Title: N 찍기, Time: 928 ms, Memory: 29288 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,13 +1,17 @@
+import java.io.*;
 import java.util.*;
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
         int i = 1;
-        while (i <= n) {
+        while(i<=n){
             System.out.println(i);
             i++;
         }
-    }
 }
+}
+
