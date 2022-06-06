580183006cc2b5ee546908736c7eabb5e0aba5a5
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-06-07 02:58:53 +0900

    [Bronze III] Title: 기찍 N, Time: 1076 ms, Memory: 33596 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        while(0<n){
+            System.out.println(n);
+            n--;
+        }
+
+    }
+}
