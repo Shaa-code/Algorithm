0ea958e3b9daf42ad9406b142e7c1e2b0245f289
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-06-08 03:21:10 +0900

    [Bronze III] Title: A+B - 8, Time: 280 ms, Memory: 18936 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int t = kb.nextInt();
+        int i = 0;
+        while(i<t) {
+            i++;
+            int m = kb.nextInt();
+            int n = kb.nextInt();
+            System.out.println("Case #"+i+":"+" "+ m + " + "+ n + " = " + (m+n));//Case #1: 1 + 1 = 2
+        }
+    }
+}
