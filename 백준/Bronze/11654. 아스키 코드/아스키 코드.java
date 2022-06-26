07d59db9d745a0fad53c6e4afb3710f83352bc84
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-06-26 18:16:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.println((int)kb.next().charAt(0));
+    }
+}
