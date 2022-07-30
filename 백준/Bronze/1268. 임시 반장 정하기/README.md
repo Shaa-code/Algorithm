d958c0f7715d5daf5aa9665646b9d1f5daab0d01
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-07-30 18:07:41 +0900

    [Bronze I] Title: 임시 반장 정하기, Time: 396 ms, Memory: 24536 KB -BaekjoonHub

---
+++ 백준/Bronze/1268. 임시 반장 정하기/README.md
@@ -0,0 +1,12 @@
+# [Bronze I] 임시 반장 정하기 - 1268 
+
+[문제 링크](https://www.acmicpc.net/problem/1268) 
+
+### 성능 요약
+
+메모리: 24536 KB, 시간: 396 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] arr = new int[n+1][6];
+            for(int i = 1 ; i <= n ; i++){
+                for(int j = 1; j <= 5 ; j++){
+                    arr[i][j] = kb.nextInt();
+                }
+            }
+        System.out.println(T.solution(n, arr));
+    }
+    public int solution(int n, int[][] arr) {
+        int max = Integer.MIN_VALUE,ret = 0;
+        for(int i = 1 ; i <= n ; i++){
+            int cnt = 0;
+            for(int j = 1 ; j <= n ; j++){
+                for(int k = 1 ; k <= 5 ; k++){
+                    if(arr[i][k] == arr[j][k]){
+                        cnt++;
+                        break;
+                    }
+                }
+            }if(cnt > max){
+                max = cnt;
+                ret = i;
+            }
+        }
+        return ret;
+    }
+}
