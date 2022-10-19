ec1b2322373ca41ca8da9e7bb4746905907d0561
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-10-20 05:04:20 +0900

    [Silver IV] Title: 주몽, Time: 192 ms, Memory: 16512 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -18,14 +19,24 @@ public class Main {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
+        Arrays.sort(A); // O(nlogn)
+
+        int p1 = 0;
+        int p2 = n-1;
         int cnt = 0;
-        for(int i = 0; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                if(A[i] + A[j] == m){
-                    cnt++;
-                }
+        while(p1 < p2){ // O(n)
+            if(A[p1] + A[p2] < m) {
+                p1++;
+            } else if(A[p1] + A[p2] == m){
+                cnt++;
+                p1++;
+                p2--;
+            } else{
+                p2--;
             }
         }
+
         System.out.println(cnt);
+        br.close();
     }
 }
