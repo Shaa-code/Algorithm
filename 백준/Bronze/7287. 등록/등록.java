5d5ff9c93ee6fd9612229fd4ed330682d396b095
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-10-26 15:23:13 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(60);
+        System.out.println("rtp3386");
+    }
+}
