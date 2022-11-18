31bffb9e39f511de5ab75b42cbee621e282e820b
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-11-18 22:35:16 +0900

    [Bronze I] Title: 약수들의 합, Time: 152 ms, Memory: 15056 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -5,25 +5,20 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         while (true) {
             int n = Integer.parseInt(br.readLine());
+            if(n == -1) break;
             StringBuilder sb = new StringBuilder();
-            int sum = 0;
-            sb.append(n + " = ");
-            for (int i = 1; i < n; i++) {
-                if (n % i == 0) { // 1 2 3 4 6 12
-                    sum += i; // 1 2 3 4 6
-                    sb.append(i + " + ");
+            int sum = 1;
+            sb.append(n + " = 1" );
+            for (int i = 2; i <= n/2 ; i++) {
+                if (n % i == 0) { //
+                    sum += i;
+                    sb.append(" + " + i);
                 }
             }
-            if(sum == n){
-                System.out.println(sb.delete(sb.length() - 3, sb.length()));
-            }else if(n == -1){
-                break;
-            }else{
-                System.out.println(n + " is NOT perfect.");
-            }
+            if (sum == n) System.out.println(sb);
+            else System.out.println(n + " is NOT perfect.");
         }
     }
 }
