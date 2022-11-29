afa9aad30c73bf59d756bb8fef39d5e7ef79a7c3
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-11-29 10:42:52 +0900

    Update 최대 매출.java

--- Java Coding Test/Two Pointer & Sliding Window/최대 매출.java
+++ Java Coding Test/Two Pointer & Sliding Window/최대 매출.java
@@ -33,3 +33,42 @@ public class Main {
         System.out.println(max);
     }
 }
+//내 방식은 인덱스를 슬라이딩 시키는 방식이었다.
+//이 방식은, 배열의 범위가 벗어나는것을 제어해주어야 하므로, 코드가 늘어난다.
+//훨씬 깔끔하게 처리할 수 있는 방법이있다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for(int i = 0; i < m ; i++){
+            sum += arr[i];
+        }
+
+        int max = 0;
+        for(int i = m; i < arr.length ; i++) {
+            sum += (arr[i] - arr[i-m]);
+            max = Math.max(max,sum);
+        }
+        System.out.println(max);
+    }
+}
+
+//즉, 슬라이딩 윈도우는 내가 위에서 한것처럼 해도 되긴하지만, sum += arr[i]을 각 원소의 갯수 만큼 반복해서 더해주면 느리다.
+//그냥 맨 앞에값을 빼주고, 맨 뒤값을 더해주면 끝이다.
