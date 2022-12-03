812157d53d8bb53f7f5c26c89babc6df26b76d0d
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-12-03 12:46:07 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0; j < n-i-1; j++){
+                sb.append(' ');
+            }
+            for(int k = 0; k < 2*i+1; k++){
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
