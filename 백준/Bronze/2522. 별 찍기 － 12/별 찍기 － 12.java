d2d507a2c21980563e816ea757d1e5ccc74b082e
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-12-06 21:24:54 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 128 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -8,21 +8,13 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n-1-i; j++){
-                sb.append(" ");
-            }
-            for(int k = 0 ; k < i+1; k++){
-                sb.append("*");
-            }
+            for(int j = 0 ; j < n-1-i; j++) sb.append(' ');
+            for(int j = 0 ; j < i+1; j++) sb.append('*');
             sb.append('\n');
         }
         for(int i = 0 ; i < n-1 ; i++){
-            for(int j = 0 ; j < i+1; j++){
-                sb.append(" ");
-            }
-            for(int k = 0 ; k < n-i-1; k++){
-                sb.append("*");
-            }
+            for(int j = 0 ; j < i+1; j++) sb.append(' ');
+            for(int j = 0 ; j < n-i-1; j++) sb.append('*');
             sb.append('\n');
         }
         System.out.println(sb);
