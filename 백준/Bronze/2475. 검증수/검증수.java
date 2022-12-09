42f83428f3472f0860ba22b434543466dc541d7e
Seung Yun Sin <70310271+Shaa-code@users.noreply.github.com>
2022-12-09 22:43:55 +0900

    [Bronze V] Title: 검증수, Time: 128 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 5 ; i++) {
+            sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+        System.out.println(sum % 10);
+    }
+}
